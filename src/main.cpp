68d9ede14b7722d44975f1a92cdadcbf3ebe4c52
jormitu <nanmitu9999@gmail.com>
2020-11-22 00:29:19 +0900

    第二回_課題2

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ int crossGreen = 9;
 int button = 7;
 int crossTime = 4000; // 歩行者用横断時間
 
+void switchLights();
+
 void setup()
 {
   pinMode(mainRed, OUTPUT);
@@ -20,52 +22,46 @@ void setup()
   pinMode(crossRed, OUTPUT);
   pinMode(crossGreen, OUTPUT);
   pinMode(button, INPUT_PULLUP); // プルアップ有効
-  //digitalWrite(mainGreen, HIGH); //　車道用初期状態（緑）
-  //digitalWrite(crossRed, HIGH);  //　歩行者用初期状態（赤）
+  digitalWrite(mainGreen, HIGH); //　車道用初期状態（緑）
+  digitalWrite(crossRed, HIGH);  //　歩行者用初期状態（赤）
 }
 
 void loop()
 {
 
   int state = digitalRead(button); // 押しボタンのチェック
-  digitalWrite(crossGreen, LOW);   //　車道用初期状態（緑）
-  digitalWrite(mainRed, HIGH);
-  digitalWrite(mainYellow, LOW);
-  digitalWrite(mainGreen, LOW);
-  delay(200);
 
-  digitalWrite(mainRed, LOW);
-  digitalWrite(mainYellow, HIGH);
-  digitalWrite(mainGreen, LOW);
-  delay(200);
-
-  digitalWrite(mainRed, LOW);
-  digitalWrite(mainYellow, LOW);
-  digitalWrite(mainGreen, HIGH);
-  delay(200);
-
-  digitalWrite(mainGreen, LOW);
-  digitalWrite(crossGreen, LOW); //　車道用初期状態（緑）
-  digitalWrite(crossRed, HIGH);  //　歩行者用初期状態（赤）
-  delay(200);
-  digitalWrite(crossGreen, HIGH); //　車道用初期状態（緑）
-  digitalWrite(crossRed, LOW);    //　歩行者用初期状態（赤）
-  delay(200);
   // 押しボタンが押されたか？
-  // if()
-  // {
-  //   delay(2000); //  車道用が黄色に変わるまでの時間
-  //   switchLights();
-  // }
+  if (state == 0)
+  {
+    delay(2000); //  車道用が黄色に変わるまでの時間
+    switchLights();
+  }
 }
 
 void switchLights()
 {
+  digitalWrite(mainGreen, LOW);   //　車道用初期状態（緑）
+  digitalWrite(crossRed, HIGH);   //　歩行者用初期状態（赤）
+  digitalWrite(mainYellow, HIGH); //黄色
+  delay(3000);                    //3秒
+  digitalWrite(mainYellow, LOW);  //黄色
+
+  digitalWrite(mainRed, HIGH);    //赤(道路)
+  digitalWrite(crossRed, LOW);    //　歩行者用初期状態（赤）
+  digitalWrite(crossGreen, HIGH); //緑(歩行)
+  delay(4000);
+  digitalWrite(mainRed, HIGH); //赤(道路)
+  //digitalWrite(crossGreen, HIGH); //緑(歩行)
 
   //緑点滅10回（0.25s間隔）
-  // for(){
-
-  // }
+  for (int i = 0; i < 5; i++)
+  {
+    digitalWrite(crossGreen, HIGH); //緑
+    delay(250);
+    digitalWrite(crossGreen, LOW); //緑
+    delay(250);
+  }
 
   digitalWrite(crossRed, HIGH);  //　歩行者用赤をON
   digitalWrite(mainGreen, HIGH); //　車道用緑をON
