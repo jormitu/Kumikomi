b9bad3d5b15482d410b267bb4c18a05129589c1f
jormitu <nanmitu9999@gmail.com>
2020-12-10 12:12:47 +0900

    第7回_例題

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,45 @@
+#include <I2CLiquidCrystal.h>
+#include <Wire.h>
+
+I2CLiquidCrystal lcd(0x3c, (uint8_t)127);
+
+int sensorPin = A0; //アナログ0番ピンを指定
+int sensorValue = 0;
+
+//アナログ入力値を摂氏度℃に変換
+float modTemp(float analog_val)
+{
+  float v = 2.56;                                // 基準電圧値( V )
+  float tempC = ((v * analog_val) / 1023) * 100; // 摂氏に換算
+  return tempC;
+}
 void setup()
 {
-  pinMode(11, OUTPUT);
+  //analogReference(INTERNAL);
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.println("hello,world!!");
 }
 
 void loop()
 {
+  //温度センサ読み取り
+  // float sum = 0;
+  // for (int i = 0; i < 10; i++)
+  // {
+  //   float val = analogRead(sensorPin);
+  //   sum = sum + val;
+  //   delay(100);
+  // }
 
-  int val = analogRead(0);      //入力値読み取り
-  float f = val * 5.0 / 1023.0; //電圧値変換
-  int intensity = map(val, 0, 1023, 0, 255);
-  analogWrite(11, intensity);
-  //serial
-  Serial.print(f);
-  Serial.print(" , ");
-  Serial.println(val);
-  delay(100);
+  // float T = sum / 10;
+  // float temp = modTemp(T);
+
+  lcd.setCursor(0, 1); //2行目一列にカーソル位置を指定
+  lcd.print(millis() / 1000);
+
+  // Serial.print("Analog");
+  // Serial.print(T);
+  // Serial.print("  ℃=");
+  // Serial.println(temp);
 }
