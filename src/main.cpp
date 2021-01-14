3d150051c2a9cce5a083c0f28220ecf72291acdb
jormitu <nanmitu9999@gmail.com>
2021-01-14 12:34:00 +0900

    課題8

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,72 @@
-#include <I2CLiquidCrystal.h>
+#include <Arduino.h>
 #include <Wire.h>
 
-I2CLiquidCrystal lcd(0x3c, (uint8_t)127);
+// デバイスアドレス(スレーブ)
+uint8_t DEVICE_ADDRESS = 0x53; //SDO=GNDに接続した場合
 
-int sensorPin = A0; //アナログ0番ピンを指定
-int sensorValue = 0;
+// XYZレジスタ用のテーブル(6byte)
+uint8_t RegTbl[6];
 
-//アナログ入力値を摂氏度℃に変換
-float modTemp(float analog_val)
-{
-  float v = 2.56;                                // 基準電圧値( V )
-  float tempC = ((v * analog_val) / 1023) * 100; // 摂氏に換算
-  return tempC;
-}
 void setup()
 {
-  analogReference(INTERNAL);
   Serial.begin(9600);
-  lcd.begin(16, 2);
-  lcd.println("Hello Arduino!!");
+
+  // マスタとしてI2Cバスに接続する
+  Wire.begin();
+
+  // DATA_FORMAT(データ形式の制御)
+  Wire.beginTransmission(DEVICE_ADDRESS);
+  // DATA_FORMATのアドレス
+  Wire.write(0x31);
+  // 「最大分解能モード」 及び 「±2g」 (0x08 == 1000)
+  Wire.write(0x08);
+  // 「10bit固定分解能モード」 及び　「±16g」にする場合 (0x03 == 0011)
+  // Wire.write(0x03);
+  Wire.endTransmission();
+
+  // POWER_TCL(節電機能の制御)
+  Wire.beginTransmission(DEVICE_ADDRESS);
+  // POWER_CTLのアドレス
+  Wire.write(0x2d);
+  // 測定モードにする
+  Wire.write(0x08);
+  Wire.endTransmission();
 }
 
 void loop()
 {
-  //温度センサ読み取り
-  float sum = 0;
-  for (int i = 0; i < 10; i++)
+
+  // XYZの先頭アドレスに移動する
+  Wire.beginTransmission(DEVICE_ADDRESS);
+  Wire.write(0x32);
+  Wire.endTransmission();
+
+  // デバイスへ6byteのレジスタデータを要求する
+  Wire.requestFrom(DEVICE_ADDRESS, 6);
+
+  // 6byteのデータを取得する
+  int i;
+  for (i = 0; i < 6; i++)
   {
-    float val = analogRead(sensorPin);
-    sum = sum + val;
-    delay(100);
+    while (Wire.available() == 0)
+    {
+    }
+    RegTbl[i] = Wire.read();
   }
 
-  float T = sum / 10;
-  float temp = modTemp(T);
+  // データを各XYZの値に変換する(LSB単位)
+  int16_t x = (((int16_t)RegTbl[1]) << 8) | RegTbl[0];
+  int16_t y = (((int16_t)RegTbl[3]) << 8) | RegTbl[2];
+  int16_t z = (((int16_t)RegTbl[5]) << 8) | RegTbl[4];
 
-  lcd.setCursor(0, 1); //2行目一列にカーソル位置を指定
-  lcd.print("temp=");
-  lcd.print(temp);
-  lcd.print("deg");
+  // 各XYZ軸の加速度(m/s^2)を出力する
+  Serial.print("X : ");
+  Serial.print(x * 0.04020726); //4.1/1000*9.80665
+  Serial.print(" Y : ");
+  Serial.print(y * 0.04020726);
+  Serial.print(" Z : ");
+  Serial.print(z * 0.04020726);
+  Serial.println(" m/s^2");
+
+  delay(100);
 }
